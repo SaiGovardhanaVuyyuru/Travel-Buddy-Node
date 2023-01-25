6bd74ce867af0fd261bc82ac961a3a903e13f133
SaiGovardhana <191fa04292@gmail.com>
2023-01-25 21:19:43 +0530

    Made auth changes

---
+++ endpoints/authmiddlewares/adminAuth.js
@@ -0,0 +1,29 @@
+const { authenticateUser } = require("../../FilesStorageUtility/user");
+
+/**
+ * 
+ * @param {import("express").Request} req 
+ * @param {import("express").Response} res 
+ * @param {*} next 
+ */
+function customerAuth(req,res,next)
+{   
+    if(req.cookies.userCredentials==undefined||req.cookies.userCredentials==null)
+        {   
+            res.json({"success":false,"message":"Not Authorized","toast-classs":"bg-red"});
+            
+        }
+    let userCredentials=JSON.parse(req.cookies.userCredentials);
+    
+    if(authenticateUser(userCredentials))
+    {
+        next();
+    }
+    else
+    {   res.cookie('userCredentials',"{}",{maxAge:0});
+        res.json({"success":false,"message":"Not Authorized","toast-classs":"bg-red"});
+    }
+
+}
+
+exports.customerAuth=customerAuth;
